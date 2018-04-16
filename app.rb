30d55f31bc0d28b2118ec777f228cc61dba07732
Michael Pope <popemray1986@gmail.com>
2018-04-16 02:36:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
 
   get '/friends' do
     @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
-    
+
     # Write your code here!
     erb :friends
   end
